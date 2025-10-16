74719eb2527a130a043daa70bfd6b090e9563301
ZlatoslavaVy <zlatoslava6b@gmail.com>
2025-10-16 22:53:37 +0300

    topic4_task1

---
+++ task1.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+def get_days_from_today(date):
+    try:
+        set_date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        print("You have specified an incorrect date format.")
+        return None
+    current_date = datetime.today()     
+    days_difference = current_date - set_date
+    return days_difference.days
+print(get_days_from_today("2025-10-10")) #correct
+print(get_days_from_today("10-10-2025")) #incorrect
